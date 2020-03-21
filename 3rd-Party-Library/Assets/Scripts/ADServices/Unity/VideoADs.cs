daa6bfad416c06a7b240dc7d216874ca57799f92
HasanGanover <hasan@ganover.com>
2020-03-21 14:03:03 +0300

    [ Refactoring ] Fixed "Unity Ad Services Listener Issue".

--- 3rd-Party-Library/Assets/Scripts/ADServices/Unity/VideoADs.cs
+++ 3rd-Party-Library/Assets/Scripts/ADServices/Unity/VideoADs.cs
@@ -10,6 +10,9 @@ public class VideoADs : UnityADs
     // Implement a function for showing a video ad:
     public void ShowVideoAD()
     {
+        //Update "activeAD" Value
+        activeAD = VideoAdID;
+
         //Start "ShowVideoAdReady" Coroutine
         StartCoroutine(ShowVideoAdReady());    
     }
@@ -35,47 +38,60 @@ public class VideoADs : UnityADs
     // Called when an ad request has successfully loaded.
     public override void OnUnityAdsReady(string placementId)
     {
-        if (placementId == VideoAdID) {
-
-            //AD is Loaded, Can Be Shown.
-            //ShowVideoAD();
-
-            Debug.Log("HandleVideoAdLoaded event received");
-        }
+        // Control the activeAD value with VideoAdID
+        if (activeAD == VideoAdID)
+        {
+            if (placementId == VideoAdID)
+            {
+                Debug.Log("HandleVideoAdLoaded event received");
+            }
+        }        
     }
 
     // Called when an ad has Finished, Skipped or Failed.
     public override void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
     {
-        // Define conditional logic for each ad completion status:
-        if (showResult == ShowResult.Finished)
+        // Control the activeAD value with VideoAdID
+        if (activeAD == VideoAdID)
         {
-            // Optionally Reward the user for watching the ad to completion.
-            Debug.Log(
-                "HandleVideoAd event received to finish.");
-        }
-        else if (showResult == ShowResult.Skipped)
-        {
-            // Do not reward the user for skipping the ad.
-            Debug.Log("HandleVideoAdSkipped event received");
-        }
-        else if (showResult == ShowResult.Failed)
-        {
-           Debug.LogError("HandleVideoAdError event received");
-        }
+            // Define conditional logic for each ad completion status:
+            if (showResult == ShowResult.Finished)
+            {
+                // Optionally Reward the user for watching the ad to completion.
+                Debug.Log(
+                    "HandleVideoAd event received to finish.");
+            }
+            else if (showResult == ShowResult.Skipped)
+            {
+                // Do not reward the user for skipping the ad.
+                Debug.Log("HandleVideoAdSkipped event received");
+            }
+            else if (showResult == ShowResult.Failed)
+            {
+                Debug.LogError("HandleVideoAdError event received");
+            }
+        }     
     }
 
     // Called when an ad request be concluded by an error.
     public override void OnUnityAdsDidError(string message)
     {
-        // Log the error.
-        Debug.LogError("HandleVideoAdError event received");
+        // Control the activeAD value with VideoAdID
+        if (activeAD == VideoAdID)
+        {
+            // Log the error.
+            Debug.LogError("HandleVideoAdError event received");
+        }    
     }
 
     // Called when the end-users triggers an ad.
     public override void OnUnityAdsDidStart(string placementId)
     {
-        // Optional actions to take when the end-users triggers an ad.
-        Debug.Log("HandleVideoAdOpening event received");
+        // Control the activeAD value with VideoAdID
+        if (activeAD == VideoAdID)
+        {
+            // Optional actions to take when the end-users triggers an ad.
+            Debug.Log("HandleVideoAdOpening event received");
+        }      
     }
 }

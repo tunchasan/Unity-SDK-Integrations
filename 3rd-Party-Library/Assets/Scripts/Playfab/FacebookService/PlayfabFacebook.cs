6b3b2f89a3f35922aea1f9b909a3750f01d90be2
tunchasan <hasantunc.dev@gmail.com>
2020-03-23 03:17:41 +0300

    [ New ] Added "PLAYFAB Facebook Share* Feature to the Project.

--- 3rd-Party-Library/Assets/Scripts/Playfab/FacebookService/PlayfabFacebook.cs
+++ 3rd-Party-Library/Assets/Scripts/Playfab/FacebookService/PlayfabFacebook.cs
@@ -2,32 +2,42 @@
 using Facebook.Unity;
 using PlayFab;
 using PlayFab.ClientModels;
+using System.Collections.Generic;
 using UnityEngine;
 using LoginResult = PlayFab.ClientModels.LoginResult;
 
 public class PlayfabFacebook : MonoBehaviour
 {
     // Holds the latest message to be displayed on the screen.
-    private string _message;
+    private string _debugMessage;
 
     #region AUTHENTICATON
 
-    public void Auth()
+    private void Start()
     {
         DebugLogHandler("Initializing Facebook..."); // logs the given message and displays it on the screen using OnGUI method
 
         // This call is required before any other calls to the Facebook API. We pass in the callback to be invoked once initialization is finished
         FB.Init(OnFacebookInitialized);
+    }
 
+    //Control User's Auth. Status
+    private bool FacebookLoggedIn()
+    {
+        return FB.IsLoggedIn;
     }
 
     private void OnFacebookInitialized()
+    {     
+        // Once Facebook SDK is initialized, if we are logged in, we log out to demonstrate the entire authentication cycle.
+        if (FacebookLoggedIn())
+            FB.LogOut();
+    }
+
+    //Facebook Auth. Handler
+    public void Auth()
     {
         DebugLogHandler("Logging into Facebook...");
-
-        // Once Facebook SDK is initialized, if we are logged in, we log out to demonstrate the entire authentication cycle.
-        if (FB.IsLoggedIn)
-            FB.LogOut();
 
         // We invoke basic login procedure and pass in the callback to process the result
         FB.LogInWithReadPermissions(null, OnFacebookLoggedIn);
@@ -69,15 +79,61 @@ public class PlayfabFacebook : MonoBehaviour
     private void DebugLogHandler(string message, bool error = false)
     {
         //The Log Message
-        _message = message;
+        _debugMessage = message;
 
         //If "error" flag is true, Print the error message in Console.
         if (error)
-            Debug.LogError(_message);
+            Debug.LogError(_debugMessage);
 
         //If "error" flag is false, Print the error message in Console.
         else
-            Debug.Log(_message);
+            Debug.Log(_debugMessage);
+    }
+
+    #endregion
+
+    #region SHARE
+
+    //Function that manages Facebook Share Service
+    public void Share(string URL)
+    {
+        //Control "LoggedIn" Facebook or not ?
+        if (FacebookLoggedIn())
+        {
+            URL = "https://www.youtube.com/watch?v=BVomQtrtMTM"; // FOR TEST
+
+            //Share Request with FB.ShareLink
+            FB.ShareLink(
+
+                new System.Uri(URL), // Content URL
+
+                "", // Content Title
+
+                "", // Content Description
+
+                null,   // Content Photo URL
+
+                OnFacebookShared); // CallBack
+        }
+
+        //If we did not "LoggedIn" FB, -> Auth()
+        else { Auth(); }
+    }
+
+    //Facebook Share Callback method to catch the errors.
+    private void OnFacebookShared(IShareResult result)
+    {
+        // If result has no errors, it means we have shared the Content in Facebook successfully
+        if (result == null || string.IsNullOrEmpty(result.Error))
+        {
+            DebugLogHandler("Facebook Sharing Completed Successfully!");
+        }
+
+        else
+        {
+            // If Facebook Sharing failed, we stop the cycle with the message
+            DebugLogHandler("Facebook Sharing Failed: " + result.Error + "\n" + result.RawResult, true);
+        }
     }
 
     #endregion

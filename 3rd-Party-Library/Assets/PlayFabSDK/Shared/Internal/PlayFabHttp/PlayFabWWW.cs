ffd278d5c0b7bded8273deedb2f81db198f072ed
HasanGanover <hasan@ganover.com>
2020-03-24 17:34:06 +0300

    [ New ] Configured and Added Fucking GPGS via PlayFab for "Auth. wtih Google" Feature.

--- 3rd-Party-Library/Assets/PlayFabSDK/Shared/Internal/PlayFabHttp/PlayFabWWW.cs
+++ 3rd-Party-Library/Assets/PlayFabSDK/Shared/Internal/PlayFabHttp/PlayFabWWW.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 #else
 using UnityEngine.Experimental.Networking;
+using UnityEngine.Networking;
 #endif
 #if !UNITY_WSA && !UNITY_WP8
 using Ionic.Zlib;

11cbd084c0eaaa472fc5342f032da435a6d1e0c7
HasanGanover <hasan@ganover.com>
2020-04-08 01:49:42 +0300

    [ New ] Added new Layer to Library for Icy Tower

---
+++ 3rd-Party-Library/Assets/IcyTower/ModuleAuth_/IT_Authentication.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IT_Authentication : MonoBehaviour
+{
+    private PlayfabCustomAuth _customAuth;
+
+    private PlayfabFacebook _facebookAuth;
+
+    private PlayFabGPGS _gpgsAuth;
+
+    private string _nickName;
+     
+    private void Awake()
+    {
+        _customAuth = new PlayfabCustomAuth();
+
+        _facebookAuth = new PlayfabFacebook();
+
+        _gpgsAuth = new PlayFabGPGS();
+
+        _nickName = "";
+    }
+
+    private void Start()
+    {
+        // LoggedIn before with GPGS
+        if (_gpgsAuth.LoggedIn())
+        {
+            /// PlayFabGPGS handles automatically Login with GPGS
+        }
+
+        // Not LoggedIn before with GPGS
+        else
+        {   
+            /// Login as Guest with Unique DeviceID
+            _customAuth.AnonymousLogin(false);
+        }
+
+    }
+
+    public void LinkFacebook()
+    {
+
+    }
+
+    public void UnLinkFacebook()
+    {
+
+    }
+
+    public void LinkGooglePlay()
+    {
+
+    }
+
+    public void UnLinkGooglePlay()
+    {
+
+    }
+}

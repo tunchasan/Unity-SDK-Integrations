3b9ca6a70e23b310a37e93ed660ffdfdabc96efb
tunchasan <hasantunc.dev@gmail.com>
2020-05-02 22:24:03 +0300

    [ New ] Added Android Push Notification feature to the library

---
+++ 3rd-Party-Library/Assets/ThirdPartyLibrary/PushNotification/AndroidNotifications.cs
@@ -0,0 +1,62 @@
+﻿using Unity.Notifications.Android;
+using UnityEngine;
+
+namespace Library.PushNotification
+{
+    public class AndroidNotifications
+    {
+        private AndroidNotificationChannel channel;
+
+        private const string channelName = "N_CENTER";
+
+        private const string channelId = "CHANNEL_1";
+
+        // Create new notification channel
+        public void CreateNotificationChannel()
+        {
+            Debug.Log("Channel Creating with " + channelId);
+
+            channel = new AndroidNotificationChannel()
+            {
+                Id = channelId,
+
+                Name = channelName,
+
+                Importance = Importance.High,
+
+                Description = "Generic notifications"
+            };
+
+            AndroidNotificationCenter.RegisterNotificationChannel(channel);
+        }
+
+        // Add new notification to channel and execute
+        public void AddNotification(string titleText, string messageText, double DelayTimeAsHours, string iconId)
+        {
+            Debug.Log("Creating Notification");
+
+            var notification = new AndroidNotification()
+            {
+                Title = titleText,
+
+                Text = messageText,
+
+                FireTime = System.DateTime.Now.AddHours(DelayTimeAsHours),
+
+                SmallIcon = iconId
+            };
+
+            AndroidNotificationCenter.SendNotification(notification, channel.Id);
+        }
+
+        // Reset all notifications and it's channel
+        public void ResetAll()
+        {
+            Debug.Log("Reset Notifications....");
+
+            AndroidNotificationCenter.CancelAllNotifications();
+
+            AndroidNotificationCenter.DeleteNotificationChannel(channelId);
+        }
+    }
+}

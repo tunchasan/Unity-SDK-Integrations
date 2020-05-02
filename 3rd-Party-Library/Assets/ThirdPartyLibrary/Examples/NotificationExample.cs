af3e72142e8dab414da06c3cbf447ea05c0a370e
HasanGanover <hasan@ganover.com>
2020-05-02 22:27:02 +0300

    [ New ] Created an example script for understanding Push Notification usage

---
+++ 3rd-Party-Library/Assets/ThirdPartyLibrary/Examples/NotificationExample.cs
@@ -0,0 +1,34 @@
+﻿using Library.PushNotification;
+using UnityEngine;
+
+public class NotificationExample : MonoBehaviour
+{
+    // Notification Title
+    private const string Notification_Title = "Race Tower";
+
+    // Notification Text
+    private const string Notification_Text = "Hey! We missed you";
+
+    // Notification Icon
+    private const string Notification_SmallIconID = "icon_01";
+
+    private AndroidNotifications _notifications;
+
+    void Start()
+    {
+        _notifications = new AndroidNotifications();
+
+        _notifications.ResetAll();
+
+        _notifications.CreateNotificationChannel();
+
+        // Half minute notification FOR TEST
+        _notifications.AddNotification(Notification_Title, Notification_Text, 0.008, Notification_SmallIconID);
+        // 1 HOUR
+        _notifications.AddNotification(Notification_Title, Notification_Text, 1, Notification_SmallIconID);
+        // 7 HOUR
+        _notifications.AddNotification(Notification_Title, Notification_Text, 7, Notification_SmallIconID);
+        // 1 DAY
+        _notifications.AddNotification(Notification_Title, Notification_Text, 24, Notification_SmallIconID);
+    }
+}

657b657f2561eea5d6f57125cb5795fbc92ad754
HasanGanover <hasan@ganover.com>
2020-05-27 17:48:49 +0300

    [ New ] Added Google Play Leaderboard feature to library

---
+++ 3rd-Party-Library/Assets/GPGSLeaderboardExample.cs
@@ -0,0 +1,19 @@
+﻿using Library.GooglePlay;
+using UnityEngine;
+
+public class GPGSLeaderboardExample : MonoBehaviour
+{
+    private static long score = 0;
+
+    public void ReportScore()
+    {
+        score += 50;
+
+        GooglePlayGameService.PostToLeaderboard(score);
+    }
+
+    public void ShowLeaderboard()
+    {
+        GooglePlayGameService.ShowLeaderboardUI();
+    }
+}

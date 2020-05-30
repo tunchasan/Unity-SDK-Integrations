b71ba024f221f582af1108e9c0adc8ca3239f725
HasanGanover <hasan@ganover.com>
2020-05-30 22:00:58 +0300

    [ New ] Added an example script for GameCenter' Leaderboard feature

---
+++ 3rd-Party-Library/Assets/ThirdPartyLibrary/Services/GameCenter/Examples/IOSLeaderboardExample.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Library.GameCenter;
+
+public class IOSLeaderboardExample : MonoBehaviour
+{
+    public string leaderboardID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Leaderboard.InitializeLeaderboard(leaderboardID,
+            (success) => {
+
+                if (success)
+                {
+                    // Everything is good to go
+                }
+                else
+                {
+                    // Something went wrong
+                }
+            });
+    }
+
+    public void ReportScore()
+    {
+        Leaderboard.ReportScore(25);
+    }
+
+    public void ShowLeaderboard()
+    {
+        Leaderboard.ShowLeaderboard();
+    }
+}
